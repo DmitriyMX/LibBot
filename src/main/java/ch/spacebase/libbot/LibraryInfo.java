f89670a350394b9e9d265652f58e8f06bc4747e4
Steveice10 <Steveice10@gmail.com>
2013-11-22 21:04:27 -0800

    Update version to release 1.0.0

--- src/main/java/ch/spacebase/libbot/LibraryInfo.java
+++ src/main/java/ch/spacebase/libbot/LibraryInfo.java
@@ -3,6 +3,6 @@ package ch.spacebase.libbot;
 public class LibraryInfo {
 
 	public static final String NAME = "LibBot";
-	public static final String VERSION = "0.0.1-dev";
+	public static final String VERSION = "1.0.0";
 	
 }
