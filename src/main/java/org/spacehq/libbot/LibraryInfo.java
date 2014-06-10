a28096d5585d3b0b5e127ed76211b9b1a89fb7c6
Steven Smith <Steveice10@gmail.com>
2014-06-09 23:27:42 -0700

    Bump to snapshot version for development

--- src/main/java/org/spacehq/libbot/LibraryInfo.java
+++ src/main/java/org/spacehq/libbot/LibraryInfo.java
@@ -3,6 +3,6 @@ package org.spacehq.libbot;
 public class LibraryInfo {
 
 	public static final String NAME = "LibBot";
-	public static final String VERSION = "1.3";
+	public static final String VERSION = "1.4-SNAPSHOT";
 
 }
