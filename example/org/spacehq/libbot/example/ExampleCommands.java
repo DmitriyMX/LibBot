55bc4f958ba96c3e62a76ff303193c8a60833178
Steven Smith <Steveice10@gmail.com>
2015-07-16 23:09:20 -0700

    Format example.

--- example/org/spacehq/libbot/example/ExampleCommands.java
+++ example/org/spacehq/libbot/example/ExampleCommands.java
@@ -5,13 +5,13 @@ import org.spacehq.libbot.chat.cmd.CommandManager;
 import org.spacehq.libbot.module.Module;
 
 public class ExampleCommands {
-	@Command(aliases = { "hello" }, desc = "Says hello!", min = 2, usage = "<hi> <hi>", permission = "example.hello")
-	public void hello(Module source, CommandManager commands, String sender, String alias, String args[]) {
-		if(args[0].equals("hi") && args[1].equals("hi")) {
-			source.chat("Hello there, " + sender + "!");
-		} else {
-			source.chat(sender + ", You need to use \"hi\" as the first and second arguments!");
-			source.chat("You put: " + args[0] + ", " + args[1]);
-		}
-	}
+    @Command(aliases = { "hello" }, desc = "Says hello!", min = 2, usage = "<hi> <hi>", permission = "example.hello")
+    public void hello(Module source, CommandManager commands, String sender, String alias, String args[]) {
+        if(args[0].equals("hi") && args[1].equals("hi")) {
+            source.chat("Hello there, " + sender + "!");
+        } else {
+            source.chat(sender + ", You need to use \"hi\" as the first and second arguments!");
+            source.chat("You put: " + args[0] + ", " + args[1]);
+        }
+    }
 }
